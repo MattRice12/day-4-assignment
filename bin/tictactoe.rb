1c73b4f9e5c06fd1de0f4b8cd89394ed0f6556ea
Matt Rice <mattrice12@outlook.com>
2016-06-18 00:13:01 -0500

    Got X's to be placed in squares. Added loop for incorrect inputs.

--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -73,115 +73,107 @@
 # Symbols - I have a class of symbols. 3 symbols each with a different value (' ', x, o). These symbols go in a squares 1-9.
   # Data: 3 symbols (' ', x, o)
   # Behavior: Is placed within 1 of 9 squares
-class Values
-  attr_reader :value
-  def initialize(value)
-    @value = value
-  end
-
-  def to_s
-    "#{@symbol}"
-  end
-
-end
-
-# Squares -- I have a class of squares. 9 types of squares, each with a different value(1-9). Squares go into Board.new
-  # Data: 9 squares
-  # Behavior: holds symbols
-class Symbols
-  attr_reader :symbol
-  def initialize(symbol)
-    @symbol = symbol
-  end
-
-  def Squares
-    @symbol = []
-    [].each do |value|
-      @symbol << Values.new(value)
-    end
-  end
-
-  def to_s
-    """
-  1    |2    |3
-  #{@one}  |  #{@two}  |  #{@three}
-  _____|_____|_____
-  4    |5    |6
-  #{@four}  |  #{@five}  |  #{@six}
-  _____|_____|_____
-  7    |8    |9
-  #{@seven}  |  #{@eight}  |  #{@nine}
-       |     |
-   """
-  end
-end
+                  # class Values
+                  #   attr_reader :value
+                  #   def initialize(value)
+                  #     @value = value
+                  #   end
+                  #
+                  #   def to_s
+                  #     "#{@symbol}"
+                  #   end
+                  #
+                  # end
+                  #
+                  # # Squares -- I have a class of squares. 9 types of squares, each with a different value(1-9). Squares go into Board.new
+                  #   # Data: 9 squares
+                  #   # Behavior: holds symbols
+                  # class Symbols
+                  #   attr_reader :symbol
+                  #   def initialize(symbol)
+                  #     @symbol = symbol
+                  #   end
+                  #
+                  #   def Squares
+                  #     @symbol = []
+                  #     [" "].each do |value|
+                  #       @symbol << Values.new(value)
+                  #     end
+                  #   end
+                  #
+                  #
+                  # end
 
 
 
 # Board
   # - Data: Grid of 9 squares
   # - Behavior: holds squares
-class Squares
-  # attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight, :nine
-  def initialize
-    # @square = []
-    #   [' ', 'X', 'Y'].each do |symbol|
-    #     @square << Symbols.new(symbol)
-    #   end
-    @one = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @two = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @three = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @four = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @five = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @six = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @seven = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @eight = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-    @nine = []
-      [].each do |symbol|
-        @square << Symbols.new(symbol)
-      end
-
-  end
-
-end
-
-
 class Board
-
-
-
+  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight, :nine
   def initialize
+    @one = one
+    @two = two
+    @three = three
+    @four = four
+    @five = five
+    @six = six
+    @seven = seven
+    @eight = eight
+    @nine = nine
+
   end
 
+  def one
+    ["1"].join
+  end
 
+  def two
+    ["2"].join
+  end
 
+  def three
+    ["3"].join
+  end
 
+  def four
+    ["4"].join
+  end
+
+  def five
+    ["5"].join
+  end
+
+  def six
+    ["6"].join
+  end
+
+  def seven
+    ["7"].join
+  end
+
+  def eight
+    ["8"].join
+  end
+
+  def nine
+    ["9"].join
+  end
+
+  def to_s
+    """
+       |     |
+    #{@one}  |  #{@two}  |  #{@three}
+  _____|_____|_____
+       |     |
+    #{@four}  |  #{@five}  |  #{@six}
+  _____|_____|_____
+       |     |
+    #{@seven}  |  #{@eight}  |  #{@nine}
+       |     |
+   """
+  end
 end
-
 
 # Players
   # Data: Player, Computer
@@ -191,33 +183,6 @@ class Players
   def initialize(name)
     @name = name
   end
-
-  def prompt(msg)
-    print msg
-    gets.chomp
-  end
-
-  # def name_prompt
-  #   prompt("What is your name? > ")
-  # end
-
-  # def hal_9000
-  #   "Hal_9000"
-  # end
-
-
-
-
-#Start
-  # - each blank starts with " "
-# def board
-#   @square.each do
-
-#Player
-  # - Player auto-selects X
-
-#Computer
-  # - Computer auto-selects O
 end
 
 # Turns
@@ -236,9 +201,15 @@ end
 class Game
   def welcome
     puts "Welcome to Tic-Tac-Toe!"
+    puts
+    print "What would you like to be called? > "
+    @name = gets.chomp
   end
 
   def greeting
+    system("clear")
+    puts "Welcome to Tic-Tac-Toe!"
+    puts
     puts "Hi #{@player.name}! You will be playing #{@computer.name}."
     puts
   end
@@ -249,45 +220,76 @@ class Game
   end
 
   def start
+    system("clear")
     welcome
-    name = gets.chomp
-
-    # name = Players.new(name).name_prompt
-    @player   = Players.new(name)
+    @board = Board.new
+    @player   = Players.new(@name)
     @computer = Players.new("Hal_9000")
     greeting
-
-    @board = Board.new ### <-- not assigned to anything yet
-    move_prompt
-  end
-
-  # def name
-  #   @name = prompt("What is your name? > ")
-  # end
-
-  def move_prompt
-    @move_prompt = prompt("Please make a move > ")
-  end
-
-  def move
-    @move = move_prompt
-    case @move
-    when "1"
-      puts @one << "X"
-    when "2"
-      puts @two << "X"
+    display_board
+    loop do
+      move
+      display_board
+      system("Clear")
+      puts "Welcome to Tic-Tac-Toe!"
+      puts
+      puts "Hi #{@player.name}! You will be playing #{@computer.name}."
+      puts
+      puts @board
     end
   end
 
+  def display_board
+    puts @board
+  end
 
+  def move
+    loop do
+    move = prompt("Please make a move > ")
+      case move
+      when "1"
+        @board.one = "X"
+        break
+      when "2"
+        @board.two = "X"
+        break
+      when "3"
+        @board.three = "X"
+        break
+      when "4"
+        @board.four = "X"
+        break
+      when "5"
+        @board.five = "X"
+        break
+      when "6"
+        @board.six = "X"
+        break
+      when "7"
+        @board.seven = "X"
+        break
+      when "8"
+        @board.eight = "X"
+        break
+      when "9"
+        @board.nine = "X"
+        break
+      else
+        system("Clear")
+        puts "Welcome to Tic-Tac-Toe!"
+        puts
+        puts "Hi #{@player.name}! You will be playing #{@computer.name}."
+        puts
+        puts @board
+        puts "I'm sorry, I didn't catch that. "
+        puts "________________________________"
+        puts
+      end
+    end
 
+  end
+  # require 'pry'; binding.pry
 
-
-
-
-  # def player_move
-  #   @player.symbol = @board.move
-  # end
 end
 
 Game.new.start
